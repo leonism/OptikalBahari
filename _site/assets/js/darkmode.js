d39c4b06a2bc4dd7ce7fb22d6697356e9b1da874
gerry leo nugroho <gerryleonugroho@gmail.com>
2024-04-23 17:06:43 +0700

    refactoring

--- _site/assets/js/darkmode.js
+++ _site/assets/js/darkmode.js
@@ -1,31 +1,31 @@
-/** @format */
+// /** @format */
 
-document.addEventListener("DOMContentLoaded", function () {
-	// Get dark mode toggle switch
-	const toggle = document.getElementById("darkModeToggle");
-	// Get document body
-	const body = document.body;
+// document.addEventListener("DOMContentLoaded", function () {
+// 	// Get dark mode toggle switch
+// 	const toggle = document.getElementById("darkModeToggle");
+// 	// Get document body
+// 	const body = document.body;
 
-	// Listen for change event on toggle switch
-	toggle.addEventListener("change", function () {
-		// If toggle is checked, enable dark mode
-		if (this.checked) {
-			enableDarkMode();
-		} else {
-			// Otherwise, disable dark mode
-			disableDarkMode();
-		}
-	});
+// 	// Listen for change event on toggle switch
+// 	toggle.addEventListener("change", function () {
+// 		// If toggle is checked, enable dark mode
+// 		if (this.checked) {
+// 			enableDarkMode();
+// 		} else {
+// 			// Otherwise, disable dark mode
+// 			disableDarkMode();
+// 		}
+// 	});
 
-	// Function to enable dark mode
-	function enableDarkMode() {
-		// Add 'dark-mode' class to body
-		body.classList.add("dark-mode");
-	}
+// 	// Function to enable dark mode
+// 	function enableDarkMode() {
+// 		// Add 'dark-mode' class to body
+// 		body.classList.add("dark-mode");
+// 	}
 
-	// Function to disable dark mode
-	function disableDarkMode() {
-		// Remove 'dark-mode' class from body
-		body.classList.remove("dark-mode");
-	}
-});
+// 	// Function to disable dark mode
+// 	function disableDarkMode() {
+// 		// Remove 'dark-mode' class from body
+// 		body.classList.remove("dark-mode");
+// 	}
+// });
