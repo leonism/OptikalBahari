54161333e40f94c50531ff6e070fe0f7edc26275
gerry leo nugroho <gerryleonugroho@gmail.com>
2026-02-22 09:26:47 +0700

    feat: Implement fade-in-on-scroll animations and gradient overlays for cards, refine navbar styling, and add `og:locale` meta tag.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,32 @@
+/* global bootstrap */
+
 document.addEventListener('DOMContentLoaded', function () {
   // Initialize Bootstrap tooltips
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    // @ts-ignore
     return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})
+
+// Load images as you scroll (fade-in)
+document.addEventListener('DOMContentLoaded', function () {
+  const faders = document.querySelectorAll('.fade-in-on-scroll')
+  const appearOptions = {
+    threshold: 0.1,
+    rootMargin: '0px 0px -50px 0px',
+  }
+
+  const appearOnScroll = new IntersectionObserver(function (entries, observer) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return
+
+      entry.target.classList.add('is-visible')
+      observer.unobserve(entry.target)
+    })
+  }, appearOptions)
+
+  faders.forEach((fader) => {
+    appearOnScroll.observe(fader)
   })
 })
